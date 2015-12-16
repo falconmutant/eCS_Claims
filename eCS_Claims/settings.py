278a1181ba4a94315d083c5a9ac2a07f8a945552
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-16 12:24:28 -0600

    initial commit

--- eCS_Claims/settings.py
+++ eCS_Claims/settings.py
@@ -97,6 +97,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR + '/static_media/files/'
