c6a60cd53631e19c38c61864bf3738db611cd659
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-16 17:47:16 -0600

    initial commit

--- Claims/views.py
+++ Claims/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
-# Create your views here.
+@login_required
+def logged_in(request):
+    t = loader.get_template('claims.html')
+    c = Context({
+        'app': 'My app',
+        'user': request.user,
+        'ip_address': request.META['REMOTE_ADDR'],
+        'message': 'I am the third view.'
+    })
+    return t.render(c)
+
