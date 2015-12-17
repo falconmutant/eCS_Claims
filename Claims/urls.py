a66649aa9829c40a964305f4fa6356bda4c55ecf
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-17 17:54:58 -0600

    initial commit

--- Claims/urls.py
+++ Claims/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('',
 
 	url(r'^$' , 'django.contrib.auth.views.login',
 		{'template_name':'index.html'}, name='login'),
-	url(r'^logged_in/$', 'Claims.views.logged_in'),
+	url(r'^logged_in/$', 'app.views.logged_in'),
 
 	url(r'^cerrar/$' , 'django.contrib.auth.views.logout_then_login',
 		 name='logout')
