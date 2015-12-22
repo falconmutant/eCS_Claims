2fe86af9ac16944662fefe5200852c97d1052521
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-22 13:15:28 -0600

    initial commit

--- Claims/views.py
+++ Claims/views.py
@@ -16,6 +16,6 @@ def detalle(request):
     )
 
 def claims(request):
-	eventos = Evento.objects.all()
+		eventos = Evento.objects.all()
     return render_to_response('claims.html',
     						   RequestContext(request,locals()))
