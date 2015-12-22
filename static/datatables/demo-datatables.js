626e00dd875d5128c03b4047c7f4212601d1b627
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-22 15:09:50 -0600

    initial commit

--- static/datatables/demo-datatables.js
+++ static/datatables/demo-datatables.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
+// -------------------------------
+// Initialize Data Tables
+// -------------------------------
 
-	var oTable = $('#tabletools').dataTable({
+$(document).ready(function() {
+    $('#example').dataTable({
         "language": {
             "lengthMenu": "_MENU_",
             "sProcessing":     "Procesando...",
@@ -25,24 +28,15 @@ $(document).ready(function() {
             }
         }
     });
-
-	var tableTools = new $.fn.dataTable.TableTools( oTable, {
-        "buttons": [
-            "copy",
-            "csv",
-            "xls",
-            "pdf",
-            { "type": "print", "buttonText": "Print me!" }
-        ],
-        "sSwfPath" : "TableTools/swf/copy_csv_xls_pdf.swf"
-    });
-
-
+    //$('.dataTables_filter input').attr('placeholder','Search...');
+    $('#example_length').hide();
 
     //DOM Manipulation to move datatable elements integrate to panel
+    $('.panel-ctrls').append($('.dataTables_filter').addClass("pull-right")).find("label").addClass("panel-ctrls-center");
+    $('.panel-ctrls').append("<i class='separator'></i>");
+    $('.panel-ctrls').append($('.dataTables_length').addClass("pull-left")).find("label").addClass("panel-ctrls-center");
 
-
-    $('#panel-tabletools .panel-footer').append($(".dataTable+.row"));
-    $('.dataTables_paginate>ul.pagination').addClass("pull-right");
+    $('#footer2').append($(".dataTable+.row"));
+    $('.dataTables_paginate>ul.pagination').addClass("pull-right m0");
 
 });
