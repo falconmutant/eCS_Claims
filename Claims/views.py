53f1a277cb75f03e04a2485446dc2c2c216960f8
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-22 16:08:12 -0600

    initial commit

--- Claims/views.py
+++ Claims/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from Claims.models import *
@@ -10,10 +10,11 @@ def logged_in(request):
         context_instance=RequestContext(request)
     )
 
-def detalle(request):
-    return render_to_response('detalles.html',
-        context_instance=RequestContext(request)
-    )
+def detalle(request, id):
+	detalle = get_object_or_404(Evento, id=id)
+    	return render_to_response('detalles.html',
+        	context_instance=RequestContext(request,locals())
+    	)
 
 def claims(request):
 	eventos = Evento.objects.all()
