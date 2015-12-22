389da2883172b7d60e7712636bfbb7bc1a03c2f3
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-22 06:56:54 -0600

    initial commit

--- eCS_Claims/settings.py
+++ eCS_Claims/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Claims',
 )
 
 from django.core.urlresolvers import reverse_lazy
