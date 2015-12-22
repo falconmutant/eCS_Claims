7c4ce8a277f10e7e01a58345f4850fab57e50221
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-22 15:12:10 -0600

    initial commit

--- static/datatables/demo-datatables.js
+++ static/datatables/demo-datatables.js
@@ -3,7 +3,7 @@
 // -------------------------------
 
 $(document).ready(function() {
-    $('#example').dataTable({
+    $('#tabletools').dataTable({
         "language": {
             "lengthMenu": "_MENU_",
             "sProcessing":     "Procesando...",
@@ -35,8 +35,6 @@ $(document).ready(function() {
     $('.panel-ctrls').append($('.dataTables_filter').addClass("pull-right")).find("label").addClass("panel-ctrls-center");
     $('.panel-ctrls').append("<i class='separator'></i>");
     $('.panel-ctrls').append($('.dataTables_length').addClass("pull-left")).find("label").addClass("panel-ctrls-center");
-
-    $('#footer2').append($(".dataTable+.row"));
     $('.dataTables_paginate>ul.pagination').addClass("pull-right m0");
 
 });
