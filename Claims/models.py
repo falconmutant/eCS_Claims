1c5eac6f03a26730474778c4afd06afd6250a44e
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-23 06:26:58 -0600

    initial commit

--- Claims/models.py
+++ Claims/models.py
@@ -4,14 +4,6 @@ from django.db import models
 
 # Create your models here.
 
-class Autorizaciones(models.Model):
-   Folio = models.CharField(max_length=255)
-   IdEvento = models.ForeignKey(Evento)
-   Estatus = models.CharField(max_length=255, null=False)
-   FechaSolicitud = models.DateTimeField()
-   TipoAprobacion = models.CharField(max_length=255)
-   Comentarios = models.CharField(max_length=255, null=False)
-
 class Paciente(models.Model):
    Nombre = models.CharField(max_length=255, null=False)
    PesoEstatura = models.CharField(max_length=255, null=False)
@@ -23,16 +15,8 @@ class Medico(models.Model):
    Especialidad = models.CharField(max_length=255, null=False)
    Cedula = models.CharField(max_length=255, null=False)
 
-
-class Evento(models.Model):
-   IdPaciente = models.ForeignKey(Paciente)
-   IdMedico = models.ForeignKey(Medico)
-   Diagnostico = models.CharField(max_length=255, null=False)
-   IdCargo = models.ForeignKey(Cargo)
-   IdTipoServicio = models.ForeignKey(TipoServicio)
-   IdProveedor = models.ForeignKey(Proveedor)
-   FechaAdmision = models.DateTimeField()
-   FechaSalida = models.DateTimeField()
+class TipoCargo(models.Model):
+   Nombre = models.CharField(max_length=255, null=False)
 
 class Cargo(models.Model):
    Descripcion = models.CharField(max_length=255, null=False)
@@ -45,17 +29,31 @@ class Cargo(models.Model):
    FechaAplicacion = models.DateTimeField()
    HoraAplicacion = models.CharField(max_length=255, null=False)
 
-class CargoPorEvento(models.Model):
-   IdEvento = models.ForeignKey(Evento)
-   IdCargo = models.ForeignKey(Cargo)
+class TipoServicio(models.Model):
+   Nombre = models.CharField(max_length=255, null=False)
 
 class Proveedor(models.Model):
    Proveedor = models.CharField(max_length=255, null=False)
    Localidad = models.CharField(max_length=255, null=False)
 
-class TipoServicio(models.Model):
-   Nombre = models.CharField(max_length=255, null=False)
+class Evento(models.Model):
+   IdPaciente = models.ForeignKey(Paciente)
+   IdMedico = models.ForeignKey(Medico)
+   Diagnostico = models.CharField(max_length=255, null=False)
+   IdCargo = models.ForeignKey(Cargo)
+   IdTipoServicio = models.ForeignKey(TipoServicio)
+   IdProveedor = models.ForeignKey(Proveedor)
+   FechaAdmision = models.DateTimeField()
+   FechaSalida = models.DateTimeField()
 
-class TipoCargo(models.Model):
-   Nombre = models.CharField(max_length=255, null=False)
+class Autorizaciones(models.Model):
+   Folio = models.CharField(max_length=255)
+   IdEvento = models.ForeignKey(Evento)
+   Estatus = models.CharField(max_length=255, null=False)
+   FechaSolicitud = models.DateTimeField()
+   TipoAprobacion = models.CharField(max_length=255)
+   Comentarios = models.CharField(max_length=255, null=False)
 
+class CargoPorEvento(models.Model):
+   IdEvento = models.ForeignKey(Evento)
+   IdCargo = models.ForeignKey(Cargo)
