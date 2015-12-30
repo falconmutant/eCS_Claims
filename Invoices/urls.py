c8131d8e9ab800844d067ade2850d15b10428096
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2015-12-30 07:38:52 -0700

    initial commit

--- Invoices/urls.py
+++ Invoices/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 
 	url(r'^invoices_detalle/(?P<id>\d+)/$', 'Invoices.views.detalle'),
-	url(r'^invoices/$', 'Invoices.views.invoices'),
+	url(r'^invoice/$', 'Invoices.views.invoices'),
 	url(r'^invoices_historial/$', 'Invoices.views.historial')
 )
