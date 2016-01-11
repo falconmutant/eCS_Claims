4731c5f25a73d14f56b2d98ee355876c40ac1800
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-11 17:02:13 -0600

    binding of REST project and WEB project

--- invoices_Web/urls.py
+++ invoices_Web/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
 	url(r'^detalle_invoices/(?P<id>\d+)/$', 'invoices_Web.views.detalle'),
 	url(r'^invoice/$', 'invoices_Web.views.invoices'),
 	url(r'^historial_invoices/$', 'invoices_Web.views.historial')
-	url(r'^save_ligar/$', 'invoices_Web.views.save_ligar')
+	url(r'^ligar/$', 'invoices_Web.views.save_ligar')
 )
