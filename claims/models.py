321bd1d09443208c01aabd0f813bad65a87f58bc
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-12 12:18:30 -0600

    binding of REST project and WEB project

--- claims/models.py
+++ claims/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from invoices_Web.models import Comprobante
 
 # Create your models here.
 
