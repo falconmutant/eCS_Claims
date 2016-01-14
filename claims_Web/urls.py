67a3fc01e1f4c9d5bda825409bf2ba5ecb91020c
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-14 10:41:39 -0600

    binding of REST project and WEB project

--- claims_Web/urls.py
+++ claims_Web/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
-from claims_Web.views import *
 
 urlpatterns = patterns('',
 
-	url(r'^$', index.as_view() ,name="index"),
+
+	url(r'^$', 'claims_Web.views.index'),
 	url(r'^login/$' , 'django.contrib.auth.views.login',
 		{'template_name':'login.html'}, name='login'),
-	url(r'^logged_in/$', logged_in.as_view(),name="logged_in"),
-	url(r'^detalles/(?P<id>\d+)/$', detalle.as_view(),name="detalles"),
-	url(r'^historial_detalles/(?P<id>\d+)/$', detalle_historial,name="historial_detalles"),
-	url(r'^claims/$', claims.as_view(),name="claims"),
-	url(r'^historial/$', historial.as_view(),name="historial"),
+	url(r'^logged_in/$', 'claims_Web.views.logged_in'),
+	url(r'^detalles/(?P<id>\d+)/$', 'claims_Web.views.detalle'),
+	url(r'^historial_detalles/(?P<id>\d+)/$', 'claims_Web.views.detalle_historial'),
+	url(r'^claims/$', 'claims_Web.views.claims'),
+	url(r'^historial/$', 'claims_Web.views.historial'),
 	url(r'^cerrar/$' , 'django.contrib.auth.views.logout',
 						{'next_page': 'claims_Web.views.index'}),
-	url(r'permisos/$', permisos.as_view(),name="permisos"),
+	url(r'permisos/$', 'claims_Web.views.permisos'),
 )
