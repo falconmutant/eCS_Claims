9ad6b032e6f645457bf8d179b4dd4e7bdaefbaa9
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-14 15:19:33 -0600

    binding of REST project and WEB project

--- claims_Web/views.py
+++ claims_Web/views.py
@@ -26,15 +26,15 @@ def permisos(request):
 	
 def cargar_permisos(request):
 	if request.method == 'POST':
-	        usuario = request.POST.get('usuario')
+	        user = request.POST.get('usuario')
 	        reporte = request.POST.get('reporte')
-	        validar = Permiso.objects.filter(usuario=usuario, reporte=reporte)
+	        validar = Permiso.objects.filter(usuario=user, reporte=reporte)
 	        Almacenar = True
 	        for x in validar:
 	        	validar.delete()
 	        	Almacenar = False
 	        if Almacenar:
-	        	liga = Permiso(usuario=usuario, reporte=reporte)
+	        	liga = Permiso(usuario=user, reporte=reporte)
 	        	liga.save()
 	        response_data = {}
 	        response_data['result'] = 'Create post successful!'
