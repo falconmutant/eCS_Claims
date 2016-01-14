6996df5521e0bc631a85b8d22a721e10b625c8fc
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-14 13:44:10 -0600

    binding of REST project and WEB project

--- claims_Web/views.py
+++ claims_Web/views.py
@@ -15,18 +15,14 @@ def index(request):
     )
 
 def permisos(request):
-    usuarios = User.objects.all()
     if request.POST:
-    		usuario = request.POST.get("user")
-			reportes = Query.objects.all()
-			permisos = Permiso.objects.all().filter(usuario=usuario)
-			value=User.objects.get(id=usuario)
-			return render_to_response('explorer/usuarios.html',RequestContext(request,locals()))
+		usuario = request.POST.get("user")
+		reportes = Query.objects.all()
+		permisos = Permiso.objects.all().filter(usuario=usuario)
+		value=User.objects.get(id=usuario)
+		return render_to_response('explorer/usuarios.html',RequestContext(request,locals()))
+	usuarios = User.objects.all()
     return render_to_response('explorer/usuarios.html',RequestContext(request,locals()))
-
-
-
-def cargar_permisos(request):
 	
 
 
