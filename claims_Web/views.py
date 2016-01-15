5bb9f62044b42558110897eba73956b5bc322caa
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-15 16:02:44 -0600

    binding of REST project and WEB project

--- claims_Web/views.py
+++ claims_Web/views.py
@@ -22,26 +22,8 @@ def permisos(request):
 		value=get_object_or_404(User, id=usuario)
 		return render_to_response('explorer/usuarios.html',RequestContext(request,locals()))
 	else:
-		try:
-			user = int(request.POST.get("u"))
-	        reporte = int(request.POST.get("r"))
-	        validar = Permiso.objects.filter(usuario=user, reporte=reporte)
-	        Almacenar = True
-	        for x in validar:
-	        	validar.delete()
-	        	Almacenar = False
-	        if Almacenar:
-	        	liga = Permiso(usuario=user, reporte=reporte)
-	        	liga.save()
-	        response_data = {}
-	        response_data['result'] = 'Create post successful!'
-	        return HttpResponse(
-	            json.dumps(response_data),
-	            content_type="application/json"
-	        )
-	    except Exception, e:
-			usuarios = User.objects.all()
-    		return render_to_response('explorer/usuarios.html',RequestContext(request,locals()))
+		usuarios = User.objects.all()
+    	return render_to_response('explorer/usuarios.html',RequestContext(request,locals()))
 
 @login_required
 def logged_in(request):
