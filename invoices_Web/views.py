57b5c9a7fe0579c8db0c5c7d625c19a0fe84774d
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-19 16:45:47 -0600

    cambio de view claims

--- invoices_Web/views.py
+++ invoices_Web/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response, get_object_or_404, get_list_or_
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from invoices_Web.models import *
+from django.contrib import auth
 from claims.models import *
 import datetime
 from django.db.models import Count
