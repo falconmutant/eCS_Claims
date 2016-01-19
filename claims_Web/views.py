93e253f869b511bd19eef8c9a087803ab7dadc61
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-19 13:49:44 -0600

    modificacion en view claims

--- claims_Web/views.py
+++ claims_Web/views.py
@@ -113,7 +113,17 @@ def claims(request):
 	paciente = Paciente.objects.filter(evento_id__in=[event.id for event in evento])
 	proveedor = Proveedor.objects.filter(id__in=[event.proveedor_id for event in evento])
 	cargo = Cargo.objects.filter(evento_id__in=[event.id for event in evento])
-	
+	if request.POST:
+		if tipouser.tipo == 'M':
+			autorizacion = Autorizacion.objects.all().filter(Estatus__in=['E','R'],TipoAprobacion='1',FechaSolicitud__range=[request.POST.get("inicio"), request.POST.get("fin")])
+		if tipouser.tipo == 'P':
+			autorizacion = Autorizacion.objects.all().filter(Estatus__in=['A','P'],TipoAprobacion='1',FechaSolicitud__range=[request.POST.get("inicio"), request.POST.get("fin")])
+		if tipouser.tipo == 'E':
+			autorizacion = Autorizacion.objects.all().filter(Estatus__in=['E','R','A','P'],TipoAprobacion='1',FechaSolicitud__range=[request.POST.get("inicio"), request.POST.get("fin")])
+		if tipouser.tipo == 'S':
+			autorizacion = Autorizacion.objects.all().filter(Estatus__in=['E','R','A','P'],TipoAprobacion='1',FechaSolicitud__range=[request.POST.get("inicio"), request.POST.get("fin")])	
+		inicio = request.POST.get("inicio")
+		fin = request.POST.get("fin")
 	return render_to_response('claims/claims.html',RequestContext(request,locals()))
 
 @login_required
