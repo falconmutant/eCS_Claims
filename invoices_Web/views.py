793d65ec589d400b52f4251df0451715fb204d59
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-20 13:10:18 -0600

    cambio para solucionar vista datatable

--- invoices_Web/views.py
+++ invoices_Web/views.py
@@ -70,7 +70,6 @@ def detalle(request, id):
 
 		return render_to_response('invoices/detalles.html',RequestContext(request,locals()))
 	except Exception, e:
-		bug += ', '+e
 		print(e)
 		return render_to_response('invoices/detalles.html',RequestContext(request,locals()))
 
