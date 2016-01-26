14423e510ca151469acd413f4dad3809ac0d5b7d
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-26 10:41:38 -0600

    solucion datatable detalle invoices

--- invoices_Web/views.py
+++ invoices_Web/views.py
@@ -50,7 +50,7 @@ def detalle(request, id):
 		proveedor = get_object_or_404(Proveedor, rfc=emisor.rfc)
 		CE = ComprobanteEvento.objects.all().filter(comprobante=id)
 		evento = Evento.objects.filter(proveedor_id=proveedor.id).exclude(id__in=[CompEvent.evento for CompEvent in CE])
-		paciente =  Paciente.objects.filter(evento_id__in=[CompEvent.evento for CompEvent in CE])
+		paciente =  Paciente.objects.all().filter(evento_id__in=[CompEvent.evento for CompEvent in CE])
 		
 		bug = 'pacientes:'
 		for pa in paciente:
