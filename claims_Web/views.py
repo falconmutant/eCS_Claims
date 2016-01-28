f7c24317f912732e7256567da7da6f3bea8da12e
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-28 09:44:50 -0600

    cambio en permiso de reportes

--- claims_Web/views.py
+++ claims_Web/views.py
@@ -21,21 +21,21 @@ def permisos(request):
 
 def save_permission(request):
     if request.method == 'POST':
-	        user = request.POST.get("user")
-			query = int(request.POST.get("idquery"))
-			selected = int(request.POST.get("selected"))
-			permiso = get_object_or_404(Permiso,usuario=user, reporte=query)
-	        if selected == 0:
-	        	liga = Permiso(usuario=user, reporte=query)
-	        	liga.save()
-	        else:
-	        	permiso.delete()
-	        response_data = {}
-	        response_data['result'] = 'Create post successful! '
-	        return HttpResponse(
-	            json.dumps(response_data),
-	            content_type="application/json"
-	        )
+    	user = request.POST.get("user")
+		query = int(request.POST.get("idquery"))
+		selected = int(request.POST.get("selected"))
+		permiso = get_object_or_404(Permiso,usuario=user, reporte=query)
+        if selected == 0:
+        	liga = Permiso(usuario=user, reporte=query)
+        	liga.save()
+        else:
+        	permiso.delete()
+        response_data = {}
+        response_data['result'] = 'Create post successful! '
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
     else:
         return HttpResponse(
             json.dumps({"nothing to see": "this isn't happening"}),
