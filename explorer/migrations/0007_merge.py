194d078d0f9f74f6995fdfbbb325d830ecf21b60
gvaldez81 <gvaldez@gmail.com>
2016-01-29 14:51:17 -0600

    Renombrado de Tablas

---
+++ explorer/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('explorer', '0006_auto_20160128_1653'),
+        ('explorer', '0006_auto_20160129_1723'),
+    ]
+
+    operations = [
+    ]
