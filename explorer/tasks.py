4eb9150f403f16e4e403c2f51ee2bb763c06d645
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-29 13:21:47 -0600

    intento de arreglar envio de email reportes

--- explorer/tasks.py
+++ explorer/tasks.py
@@ -20,8 +20,8 @@ else:
 
 @task
 def execute_query(query_id, email_address):
-    q = Query.objects.get(pk=query_id)
     return 'paso'
+    q = Query.objects.get(pk=query_id)
     r = csv_report(q)
     random_part = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
     resp = _upload('%s.csv' % random_part, r)
