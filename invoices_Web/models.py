8c910151a9cd359db2c73addf55e9bb97478be1a
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-02-01 17:03:49 -0600

    cambio en pantalla detalles invoices

--- invoices_Web/models.py
+++ invoices_Web/models.py
@@ -81,10 +81,10 @@ class Impuesto(models.Model):
 
 class ComprobanteTipo(models.Model):
 	tipo = models.CharField(choices= LEVEL_TIPO, max_length=1)
-	Fecha = models.DateTimeField()
+	Fecha = models.DateField()
 	Usuario = models.ForeignKey(User)
 	comprobante = models.IntegerField()
-	
+
 class ComprobanteEvento(models.Model):
 	comprobante = models.IntegerField()
 	evento = models.IntegerField()
