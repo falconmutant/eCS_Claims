0e4ed63cf6a74cad7f252803cd21d56fa1259f34
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-02-01 16:59:55 -0600

    cambio en pantalla detalles invoices

--- invoices_Web/views.py
+++ invoices_Web/views.py
@@ -96,8 +96,8 @@ def save_level(request):
     	x = datetime.datetime.now()
     	if x.month < 10:
     		fecha = "%s-0%s-%s"% (x.year, x.month, x.day)
-		else:
-			fecha = "%s-%s-%s"% (x.year, x.month, x.day)
+    	else:
+    		fecha = "%s-%s-%s"% (x.year, x.month, x.day)
     	comprobante = int(request.POST.get('comprobante'))
         tipo = request.POST.get('tipo')
 
