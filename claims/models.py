9c127ef497e13d2beb1a001f292a4b8a3035473f
Gerri <gvaldez@gmail.com>
2016-02-02 14:41:02 -0600

    Telegram create contact

--- claims/models.py
+++ claims/models.py
@@ -82,7 +82,7 @@ class Proveedor(models.Model):
    whatsapp = models.CharField(choices= YES_NO,max_length=1,default='N')
    telegram = models.CharField(choices= YES_NO,max_length=1,default='N')
    sms = models.CharField(choices= YES_NO,max_length=1,default='N')
-   tgContact = models.CharField(max_length=255, blank=True)
+   tgcontact = models.CharField(max_length=255, blank=True)
    def __str__(self):
       return "%s - %s" % (self.rfc, self.localidad)
 
