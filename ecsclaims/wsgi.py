5ab0e06d3c4efcd62eabcc0c0c97e64298acb27e
Gerri <gvaldez@gmail.com>
2016-02-02 12:15:19 -0600

    Global Gmail Api

--- ecsclaims/wsgi.py
+++ ecsclaims/wsgi.py
@@ -30,7 +30,7 @@ home_dir = os.path.expanduser('~')
 credential_dir = os.path.join(home_dir, '.credentials')
 if  os.path.exists(credential_dir):
 	credential_path = os.path.join(credential_dir,'gmail-python-quickstart.json')
-    store = oauth2client.file.Storage(credential_path)
+    #store = oauth2client.file.Storage(credential_path)
     #credentials = store.get()
     #if credentials and not credentials.invalid:
     #	http = credentials.authorize(httplib2.Http())
