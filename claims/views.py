d0fbf74398b3d5c5f34634a0016baf893e92ebea
Gerri <gvaldez@gmail.com>
2016-02-02 01:15:26 -0600

    Envio de Alertas

--- claims/views.py
+++ claims/views.py
@@ -245,7 +245,7 @@ class EventosView(ProveedorView):
             usuariosLoc = UsuarioLocalidad.objects.filter(localidad_id=localidad.id)
             logger.error(usuariosLoc)
             params = {}
-            for user in usuariosLoc.user.all()
+            for user in usuariosLoc.user_set.all()
                 print(user)
                 userData = TipoUsuario.objects.get(user=user.id)
                 print(userData)
