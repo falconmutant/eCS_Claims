b56afd97c82431f14f2b608d0f01d11077eaa7a9
Rodrigo Piña Enriquez <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-02-05 13:09:06 -0600

    prueba de whatsapp 1

--- claims/views.py
+++ claims/views.py
@@ -8,8 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist
 #from app.permissions import is_app
 from .models import *
 from .serializers import *
-#from .utils import secret_key_gen, get_url, sendNotifications
-from .utils import get_url
+from .utils import secret_key_gen, get_url, sendNotifications
 from .permissions import ProveedorView, EventoView
 import json
 import datetime 
--- claims_Web/views.py
+++ claims_Web/views.py
@@ -9,7 +9,7 @@ from django.views.generic import ListView
 from claims.models import *
 from invoices_Web.models import *
 from explorer.models import *
-#from claims.utils import sendNotifications
+from claims.utils import sendNotifications
 import datetime
 
 
